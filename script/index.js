767dd9259954150e02fd3db86b95d1f86481d458
Dmitry <dmitry@Dmitry99.org>
2019-10-11 18:18:14 +0300

    Continue work on gallery

--- script/index.js
+++ script/index.js
@@ -10,3 +10,81 @@ function addScroll (element) {
 
 addScroll(projectsLink);
 
+
+class popupInfo {
+    constructor (data) {
+        this.pictureUrl = data.pictureUrl;
+        this.title = data.title;
+        this.description = data.description;
+        this.linkUrl = data.linkUrl;
+    }
+
+    fillPopup(element) {
+        let pictureContainer = element.querySelector('.projects-popup-picture');
+        pictureContainer.style.background = `center / 100% 100% no-repeat url(${this.pictureUrl})`;
+        
+        let popupTitle = element.querySelector('.projects-popup-title');
+        popupTitle.innerHTML = this.title;
+
+        let popupDescription = element.querySelector('.projects-popup-description');
+        popupDescription.innerHTML = this.description;
+
+        let popupLink = element.querySelector('.projects-popup-link');
+        popupLink.href = this.linkUrl;
+    }
+}
+
+let dataPopup = [];
+
+let lenomaPopup = new popupInfo({
+    pictureUrl: '../static/pct/lenoma-preview.png',
+    title: 'Главная страница интернет-магазина',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat adipisci esse ipsum odio deleniti nulla maiores deserunt ratione quis commodi reiciendis, iure, magnam quisquam dolore amet, quaerat voluptas nesciunt vero?',
+    linkUrl: '../static/projects/lenoma/lenoma.html'
+});
+
+let fusionPopup = new popupInfo({
+    pictureUrl: '../static/pct/fusion-preview.png',
+    title: 'Портфолио компании',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat adipisci esse ipsum odio deleniti nulla maiores deserunt ratione quis commodi reiciendis, iure, magnam quisquam dolore amet, quaerat voluptas nesciunt vero?',
+    linkUrl: '../static/projects/fusion/fusion.html'
+});
+
+let landingPopup = new popupInfo({
+    pictureUrl: '../static/pct/landing-preview.png',
+    title: 'Лэндинг №1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat adipisci esse ipsum odio deleniti nulla maiores deserunt ratione quis commodi reiciendis, iure, magnam quisquam dolore amet, quaerat voluptas nesciunt vero?',
+    linkUrl: '../static/projects/landing/landing.html'
+});
+
+dataPopup.push(lenomaPopup);
+dataPopup.push(fusionPopup);
+dataPopup.push(landingPopup);
+
+let closeButton = document.querySelector('#btn-project-close');
+let projectPopup = document.querySelector('.projects-popup');
+let projectCard = document.querySelectorAll('.projects-gallery-item');
+
+function closePopup (element) {
+    element.addEventListener('click', () => {
+        projectPopup.style.display = 'none';
+    });
+}
+
+function showPopup (element, dataPopup) {
+    element.addEventListener('click', () => {
+        projectPopup.style.display = 'flex';
+        dataPopup.fillPopup(projectPopup);
+    });
+}
+
+for(let i = 0; i < projectCard.length; i++) {
+    showPopup(projectCard[i], dataPopup[i]);
+    
+}
+
+closePopup(closeButton);
+
+
+
+
